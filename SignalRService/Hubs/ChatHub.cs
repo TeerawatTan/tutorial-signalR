03e5a07d416d4a5d61af195d3d7bd9ef96cda2d4
teerawat.t1 <teerawat.tanatan@gmail.com>
2023-07-19 21:51:52 +0700

    first commit

---
+++ SignalRService/Hubs/ChatHub.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+        private readonly IDictionary<string, UserConnectionModel> _connections;
+
+        public ChatHub(IDictionary<string, UserConnectionModel> connections)
+        {
+            _botUser = "Chat Bot";
+            _connections = connections;
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnectionModel userConnection))
+            {
+                _connections.Remove(Context.ConnectionId);
+                Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has left");
+                SendUsersConnected(userConnection.Room);
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task JoinRoom(UserConnectionModel userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            _connections[Context.ConnectionId] = userConnection;
+
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has joined {userConnection.Room}");
+
+            await SendUsersConnected(userConnection.Room);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnectionModel userConnection))
+            {
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+
+        public Task SendUsersConnected(string room)
+        {
+            var users = _connections.Values
+                .Where(c => c.Room == room)
+                .Select(c => c.User);
+
+            return Clients.Group(room).SendAsync("UsersInRoom", users);
+        }
+    }
+}
---
+++ SignalRService/Hubs/UserConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRService.Hubs
+{
+    public class UserConnectionModel
+    {
+        public string User { get; set; }
+        public string Room { get; set; }
+    }
+}
